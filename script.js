166c522a392b06363a258e20a195b621757ee3b1
Andrew Chernigovsky <chernigovsky108@gmail.com>
2022-01-14 18:01:26 +0300

    1

--- script.js
+++ script.js
@@ -4,72 +4,13 @@ const form = document.forms.form,
 			password = form.pass,
 			passwordCheck = form.passCheck,
 			username = form.username,
+			file = form.file,
+			comment = form.comment,
+			dataUser = form.dataUser,
 			inputs = document.querySelectorAll('.inputForm');
 
 let emailValue = email.value;
-// let phoneValue = phone.value;
-let classError = 'error';
-
-// const errorText = 'Проверьте символы ';
-
-// form.addEventListener('submit', function(evt) {
-// 	if (emailTest(email)) {
-// 		email.parentElement.insertAdjacentHTML(
-// 			"beforeend",
-// 			`<div class="${classError}">
-// 				Введите email
-// 			</div>`
-// 		);
-// 		email.style.border = "1px solid red";
-// 		evt.preventDefault();
-// 	} else {
-// 		email.style.border = "1px solid green";
-// 		evt.preventDefault();
-// 	}
-
-// 	// if (phoneTest(phone)) {
-// 	// 	phone.parentElement.insertAdjacentHTML(
-// 	// 		"beforeend",
-// 	// 		`<div class="${classError}">
-// 	// 			Введите phone
-// 	// 		</div>`
-// 	// 	);
-// 	// 	phone.style.border = "1px solid red";
-// 	// 	evt.preventDefault();
-// 	// } else {
-// 	// 	phone.style.border = "1px solid green";
-// 	// }
-// });
-
-// email.addEventListener('focus', function(evt) {
-// 	if (email.nextElementSibling) {
-// 		email.nextElementSibling.remove();
-// 	}
-// 	// if (phone.nextElementSibling) {
-// 	// 	phone.nextElementSibling.remove();
-// 	// }
-// });
-
-// form.addEventListener('click', function(evt)  {
-// 	if (email.nextElementSibling) {
-// 		email.nextElementSibling.remove();
-// 	}
-// 	// if (phone.nextElementSibling) {
-// 	// 	phone.nextElementSibling.remove();
-// 	// }
-// });
-
-// function emailTest(email) {
-// 	return /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(emailValue);
-// };
-
-// function phoneTest(phone) {
-// 	return !/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(phoneValue);
-// };
-
-///////////////
-/////////////////
-
+let phoneValue = phone.value;
 
 
 form.addEventListener('submit', function(evt) {
@@ -84,12 +25,39 @@ form.addEventListener('submit', function(evt) {
 				"beforeend",
 				`<span class="errorText">поле должно быть заполнено</span>`
 			);
+		evt.preventDefault();
+
+		} else {
+			if (el.classList.contains('error')) {
+				el.classList.remove('error');
+				el.classList.add('success');
+			}
+		}
+	});
+
+		if(!dataUser.checked){
+			let dataField = document.querySelector('.datauser-fieldset');
+
+			if (dataUser.nextElementSibling){
+				dataUser.nextElementSibling.remove()
+			}
+
+			dataField.classList.add('error');
+			dataUser.parentElement.insertAdjacentHTML(
+						"beforeend",
+						`<span class="errorText">Это обязательный пункт</span>`
+					);
 			evt.preventDefault();
 		} else {
 			if (el.classList.contains('error')) {
 				el.classList.remove('error');
 				el.classList.add('success');
 			}
+
+			if (dataUser.nextElementSibling){
+				dataUser.nextElementSibling.remove()
+			}
+		}
 
 			if (isValidEmail(email)){
 				if (email.nextElementSibling){
@@ -101,6 +69,7 @@ form.addEventListener('submit', function(evt) {
 					"beforeend",
 					`<span class="errorText">Неккоректно заполнено</span>`
 				);
+				evt.preventDefault();
 			} else {
 				if (email.nextElementSibling){
 					email.nextElementSibling.remove()
@@ -119,6 +88,7 @@ form.addEventListener('submit', function(evt) {
 					"beforeend",
 					`<span class="errorText">Неккоректно заполнено</span>`
 				);
+				evt.preventDefault();
 			} else {
 				if (phone.nextElementSibling){
 					phone.nextElementSibling.remove()
@@ -126,9 +96,47 @@ form.addEventListener('submit', function(evt) {
 				phone.classList.remove('error');
 				phone.classList.add('success');
 			}
-		}
-	});
-	evt.preventDefault();
+
+			if (isValidName(username)){
+				if (username.nextElementSibling){
+					username.nextElementSibling.remove()
+				}
+				username.classList.remove('success');
+				username.classList.add('error');
+				username.parentElement.insertAdjacentHTML(
+					"beforeend",
+					`<span class="errorText">Неккоректно заполнено</span>`
+				);
+				evt.preventDefault();
+			} else {
+				if (username.nextElementSibling){
+				username.nextElementSibling.remove()
+				}
+				username.classList.remove('error');
+				username.classList.add('success');
+			}
+
+			if (password.value != passwordCheck.value){
+				if (passwordCheck.nextElementSibling){
+					passwordCheck.nextElementSibling.remove()
+				}
+				password.classList.remove('success');
+				passwordCheck.classList.remove('success');
+				passwordCheck.classList.add('error');
+				passwordCheck.parentElement.insertAdjacentHTML(
+					"beforeend",
+					`<span class="errorText">Пароли не совпадают</span>`
+				);
+				evt.preventDefault();
+			} else {
+				if (passwordCheck.nextElementSibling){
+					passwordCheck.nextElementSibling.remove()
+				}
+				passwordCheck.classList.remove('error');
+				if (passwordCheck.value != '') {
+					passwordCheck.classList.add('success');
+				}
+			}
 });
 
 email.addEventListener('focus', () => {
@@ -147,3 +155,26 @@ function isValidEmail(input){
 function isValidPhone(input){
 	return !/^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/.test(input.value);
 };
+
+function isValidName(input){
+	return !/^\D{1,3}[А-Яа-яA-Za-z0-9_-]{3,30}$/.test(input.value);
+};
+
+file.addEventListener('change', function (e){
+	let selectedFile = file.files[0];
+
+	let fileURL = URL.createObjectURL(selectedFile);
+
+	file.parentElement.insertAdjacentHTML(
+		"beforeend",
+		`<div class="file-image">
+			<img alt="userpic" title="${selectedFile.name}" src="${fileURL}">
+		</div>
+		`
+	);
+});
+
+comment.addEventListener("paste", function (event) {
+	event.preventDefault();
+	alert('Вставка запрещена');
+});
